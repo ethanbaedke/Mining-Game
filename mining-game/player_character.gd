492b0d74491c69d7bece6462ad58b862ce4d0d43
Ethan Baedke <110857520+ethanbaedke@users.noreply.github.com>
2026-02-12 12:27:27 -0600

    Updated miner sprite and code
    Added a sprite for each of the four directions the miner could face and code to set these sprites during movement.

--- mining-game/player_character.gd
+++ mining-game/player_character.gd
@@ -1,5 +1,12 @@
 class_name PlayerCharacter extends CharacterBody2D
 
+@onready var _sprite_2d:Sprite2D = $Sprite2D
+
+const PI_OVER_FOUR:float = PI * 0.25
+const THREE_PI_OVER_FOUR:float = (PI * 3.0) * 0.25
+const FIVE_PI_OVER_FOUR:float = (PI * 5.0) * 0.25
+const SEVEN_PI_OVER_FOUR:float = (PI * 7.0) * 0.25
+
 const MOVE_SPEED:int = 40
 
 func _physics_process(delta: float) -> void:
@@ -12,4 +19,35 @@ func _physics_process(delta: float) -> void:
 	# Normalize input to ensure diagonal movement is the same speed as lateral movement
 	self.velocity = input.normalized() * MOVE_SPEED
 
+	_update_sprite_direction()
+
 	move_and_slide()
+
+# Updates the direction the sprite is facing based on the players velocity
+func _update_sprite_direction() -> void:
+	
+	if (velocity.is_zero_approx()):
+		return
+	
+	# This puts our angle in the range (0, 2*PI), starting at PI on the unit circle, going counter-clockwise
+	var angle:float = self.velocity.angle() + PI
+	
+	# Slightly widen our left and right margins to force diagonal movement to be displayed horizontally
+	const WIDEN_MODIFIER:float = 0.0000001 # This value was derived through testing
+	const LEFT_MARGIN_START:float = SEVEN_PI_OVER_FOUR - WIDEN_MODIFIER
+	const DOWN_MARGIN_START:float = FIVE_PI_OVER_FOUR + WIDEN_MODIFIER
+	const RIGHT_MARGIN_START:float = THREE_PI_OVER_FOUR - WIDEN_MODIFIER
+	const UP_MARGIN_START:float = PI_OVER_FOUR + WIDEN_MODIFIER
+	
+	# Our angle calculation above makes it easy for us to check which direction we should face
+	var atlas_texture:AtlasTexture = _sprite_2d.texture
+	if (angle > LEFT_MARGIN_START):
+		atlas_texture.region.position.x = 32.0
+	elif (angle > DOWN_MARGIN_START):
+		atlas_texture.region.position.x = 0.0
+	elif (angle > RIGHT_MARGIN_START):
+		atlas_texture.region.position.x = 16.0
+	elif (angle > UP_MARGIN_START):
+		atlas_texture.region.position.x = 48.0
+	else:
+		atlas_texture.region.position.x = 32.0
