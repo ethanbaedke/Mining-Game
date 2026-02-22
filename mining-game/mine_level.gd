b8d659e21673d3b96b3487abd923c469ca862a93
Ethan Baedke <110857520+ethanbaedke@users.noreply.github.com>
2026-02-21 19:15:11 -0600

    Generating map with noise.

--- mining-game/mine_level.gd
+++ mining-game/mine_level.gd
@@ -1,7 +1,10 @@
 class_name MineLevel extends Node2D
 
-@onready var wallPhysicalLayer:TileMapLayer = $WallPhysicalLayer
-@onready var wallVisualLayer:TileMapLayer = $WallVisualLayer
+@onready var wall_physical_layer:TileMapLayer = $WallPhysicalLayer
+@onready var wall_visual_layer:TileMapLayer = $WallVisualLayer
+
+const MAP_WIDTH:int = 64
+const MAP_HEIGHT:int = 64
 
 const CELL_NEIGHBORS:Array[TileSet.CellNeighbor] = [
 	TileSet.CellNeighbor.CELL_NEIGHBOR_RIGHT_SIDE,
@@ -17,27 +20,48 @@ const CELL_NEIGHBORS:Array[TileSet.CellNeighbor] = [
 func remove_tile(cell_coordinates:Vector2i) -> void:
 	
 	# This call removes the tile at the input cell coordinates and updates the tiles of the surrounding cells using their terrain.
-	wallPhysicalLayer.set_cells_terrain_connect([cell_coordinates], 0, -1)
+	wall_physical_layer.set_cells_terrain_connect([cell_coordinates], 0, -1)
 	
 	# Update the visual tilemap to reflect changes in the physical tilemap.
 	_update_visual_tilemap_cell(cell_coordinates)
 	for neighbor:TileSet.CellNeighbor in CELL_NEIGHBORS:
-		var neighbor_coords:Vector2i = wallVisualLayer.get_neighbor_cell(cell_coordinates, neighbor)
-		if (wallVisualLayer.get_cell_source_id(neighbor_coords) != -1):
+		var neighbor_coords:Vector2i = wall_visual_layer.get_neighbor_cell(cell_coordinates, neighbor)
+		if (wall_visual_layer.get_cell_source_id(neighbor_coords) != -1):
 			_update_visual_tilemap_cell(neighbor_coords)
 
 func _ready() -> void:
 	
-	# Place visual tiles for all physical tiles present in the wallPhysicalLayer.
-	var used_cell_coords:Array[Vector2i] = wallPhysicalLayer.get_used_cells()
+	_generate_map()
+	
+	# Place visual tiles for all physical tiles present in the wall_physical_layer.
+	var used_cell_coords:Array[Vector2i] = wall_physical_layer.get_used_cells()
 	for cell_coords:Vector2i in used_cell_coords:
 		_update_visual_tilemap_cell(cell_coords)
+
+# Generates the map. Places the physical tiles on the map.
+func _generate_map() -> void:
+	
+	# Use FastNoiseLite for our noise algorithm.
+	var noise:FastNoiseLite = FastNoiseLite.new()
+	noise.noise_type = FastNoiseLite.TYPE_CELLULAR
+	noise.cellular_return_type = FastNoiseLite.RETURN_DISTANCE2_SUB
+	noise.frequency = 0.04
+	
+	# Generate a random seed for the noise alogirithm.
+	var rng:RandomNumberGenerator = RandomNumberGenerator.new()
+	noise.seed = rng.randi()
+	
+	# Place physical tiles.
+	for y:int in range(MAP_HEIGHT):
+		for x:int in range(MAP_WIDTH):
+			if (noise.get_noise_2d(x, y) < -0.7):
+				wall_physical_layer.set_cells_terrain_connect([Vector2i(x, y)], 0, 0)
 
 # Updates a cell on the visual tilemap to match the cooresponding cell on the physical tilemap.
 func _update_visual_tilemap_cell(cell_coordinates:Vector2i) -> void:
 	
 	# Get the TileSet coordinates of the tile present on the physical tilemap at the input cell coordinates.
-	var atlas_coords:Vector2i = wallPhysicalLayer.get_cell_atlas_coords(cell_coordinates)
+	var atlas_coords:Vector2i = wall_physical_layer.get_cell_atlas_coords(cell_coordinates)
 	
 	# Set the visual tilemap to use the same tile on the same cell, but from it's own TileSet.
-	wallVisualLayer.set_cell(cell_coordinates, 1, atlas_coords)
+	wall_visual_layer.set_cell(cell_coordinates, 1, atlas_coords)
